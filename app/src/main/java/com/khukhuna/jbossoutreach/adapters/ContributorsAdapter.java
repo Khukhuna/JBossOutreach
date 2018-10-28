beaaa30f68db5df527911108c6f70a4d7003e1e1
FiNkacho <sabaxuxunashvili@gmail.com>
2018-10-28 21:41:50 +0400

    Details page

--- app/src/main/java/com/khukhuna/jbossoutreach/adapters/ContributorsAdapter.java
+++ app/src/main/java/com/khukhuna/jbossoutreach/adapters/ContributorsAdapter.java
@@ -1,5 +1,8 @@
 package com.khukhuna.jbossoutreach.adapters;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +15,8 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.khukhuna.jbossoutreach.R;
 import com.khukhuna.jbossoutreach.models.Contributor;
+import com.khukhuna.jbossoutreach.network.GitHubClient;
+import com.khukhuna.jbossoutreach.network.GitHubService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,13 +38,51 @@ public class ContributorsAdapter extends RecyclerView.Adapter<ContributorsAdapte
             name = v.findViewById(R.id.contribName);
         }
 
-        public void bind(Contributor contributor) {
+        public void bind(final Contributor contributor) {
             Glide.with(view.getContext())
                     .load(contributor.getImageurl())
                     .apply(new RequestOptions().circleCrop())
                     .into(avatar);
             name.setText(contributor.getName());
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showDialog(view, contributor);
+                }
+            });
+        }
 
+        public void showDialog(View view, final Contributor contributor){
+            final Dialog dialog = new Dialog(view.getContext());
+
+            dialog.setContentView(R.layout.contributor_detail);
+
+            TextView name = dialog.findViewById(R.id.details_name);
+            ImageView image = dialog.findViewById(R.id.details_image);
+            TextView contributions = dialog.findViewById(R.id.details_contrib);
+            ImageView exit = dialog.findViewById(R.id.detail_exit);
+
+            name.setText(contributor.getName());
+            Glide.with(view.getContext()).load(contributor.getImageurl()).apply(new RequestOptions().circleCrop()).into(image);
+            contributions.setText(view.getResources().getString(R.string.contributions_x).replace("X", String.valueOf(contributor.getContribution())));
+
+            image.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://github.com/"+contributor.getName()));
+                    view.getContext().startActivity(browserIntent);
+                }
+            });
+
+            exit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.hide();
+                    dialog.cancel();
+                }
+            });
+
+            dialog.show();
         }
     }
 
@@ -76,4 +119,5 @@ public class ContributorsAdapter extends RecyclerView.Adapter<ContributorsAdapte
         mDataset.addAll(data);
         notifyDataSetChanged();
     }
+
 }
